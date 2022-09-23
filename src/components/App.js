eb13926f28e2bbfdba9554869ba64e33b355d8f2
Aqib Raza <40744680+itzraxa@users.noreply.github.com>
2022-09-23 15:32:13 +0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+import "./App.css";
+function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Aqib",
+      email: "aqib96.ar@outlook.com",
+    },
+    {
+      id: "2",
+      name: "Raza",
+      email: "aqib96.ar@gmail.com",
+    },
+    {
+      id: "3",
+      name: "cboy",
+      email: "aqib98.ar@outlook.com",
+    },
+  ];
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const ContactList = (props) => {
+    console.log(props);
+    return (
+      
+    <div className='ui called list'>Contact List</div>
+  )
+}
+export default ContactList ;
